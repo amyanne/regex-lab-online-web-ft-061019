1c4088c1d01fd4dce4572d40f96df4d3550eef2e
Amy Westcott <amy.westcott@yahoo.com>
2019-06-11 21:28:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
- if word.scan(/\A[aeiou][a-z]*\b/)
+ if word.scan(/\A[aeiou][a-z]*\A/)
    true 
- elsif word.scan(/\A[b-df-hj-np-tv-z][a-z]*\b/)
+ elsif word.scan(/\A[b-df-hj-np-tv-z][a-z]*\A/)
    false
  end
  
