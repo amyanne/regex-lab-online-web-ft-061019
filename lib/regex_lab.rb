f6bbb3b6f23a92498d4e9d94ef68a74d354f065b
Amy Westcott <amy.westcott@yahoo.com>
2019-06-11 21:27:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
- if word.scan(/\b[aeiou][a-z]*\b/)
+ if word.scan(/\A[aeiou][a-z]*\b/)
    true 
- elsif word.scan(/\b[b-df-hj-np-tv-z][a-z]*\b/)
+ elsif word.scan(/\A[b-df-hj-np-tv-z][a-z]*\b/)
    false
  end
  
